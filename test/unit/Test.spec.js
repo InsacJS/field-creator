f21ec4d1262919e2548ceabba56c555e2145dce5
Wilmer Alex Quispe Quispe <q.wilmer.alex@gmail.com>
2018-02-07 00:02:15 -0400

    Se configuraron las pruebas y se crearon las clases principales.

---
+++ test/unit/Test.spec.js
@@ -0,0 +1,8 @@
+/* global describe it expect */
+describe('\n - Class: Example\n', () => {
+  describe(` Method: example`, () => {
+    it('Description example', () => {
+      expect(true).to.equal(true)
+    })
+  })
+})
